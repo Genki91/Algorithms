b20bb70d3b12300712524eb63cfc3a2e186d3483
Luka <traxdater12@gmail.com>
2013-03-13 23:11:06 +0100

    Update Log2.java

--- Week 1/Log2.java
+++ Week 1/Log2.java
@@ -9,20 +9,30 @@ public class Log2
 {
     public static int lg(int N)
     {
+        //check simple base cases for N = 0, 1 or 2
         if (N == 0)
             return -1;
         if (N == 1)
             return 0;
         if (N == 2)
             return 1;
-        int difference = N - 2;
+        //if N is greater than simple base cases
+        //not needed 
+        //int difference = N - 2;
+        
+        // (log base 2 of 10 (N)) = x  -> can be written like this:
+        // (2 to the power of x) = 10 (N)  -> here x is number of times we have to multiply the base by itself to 
+        //get to the number (N) given by the user
+        //so in a simple loop we keep track i.e. check if the track has overcome N
+        //when it has, we simply return counter which is our largest integer not larger than the base2 log.
         int track = 2;
         int counter = 1;
         for (int i = 0; i < N; i++)
         {
             track *= 2;
-            if ((N - track) < difference)
-                difference = N - track;
+            //if ((N - track) < difference)
+              //  difference = N - track;
+            
             if (track > N)
                 return counter;
             counter++;
@@ -31,6 +41,8 @@ public class Log2
     }
     public static void main(String[] args)
     {
+        //take number as an argument to main
+        // and call lg static method
         int x = StdIn.readInt();
         System.out.println(lg(x));
     }
